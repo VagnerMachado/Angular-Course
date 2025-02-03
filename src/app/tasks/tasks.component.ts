4c837e1fe5e32ec475ad696c2f218c472ab50fc2
Vagner Machado <VagnerMachado84@hotmail.com>
2025-02-02 23:01:31 -0500

    add some styling and object as input

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -7,6 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) name!: string; // uses ! to stop TS from complaining about no initial value
+  @Input({ required: true }) name: string | undefined; // undefined is used to indicate that the value is not set yet, allows for multiple types
 
+  
 }

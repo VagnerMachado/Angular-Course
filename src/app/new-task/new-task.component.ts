b64f334f9f57205a31522a4ecf4dddc2ebadcb71
Vagner Machado <VagnerMachado84@hotmail.com>
2025-02-06 22:55:29 -0500

    work on dialog to close on click in bkg

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NewTaskComponent {
 
+  @Output() cancelAddTask = new EventEmitter<void>();
+
+  onCancelAddTask() {
+    this.cancelAddTask.emit();
+  }
 }

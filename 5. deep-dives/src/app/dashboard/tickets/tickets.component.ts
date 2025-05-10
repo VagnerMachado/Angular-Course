44edbaf1aaabf39d1dc0e67a9a232ff5c188688c
Vagner Machado <VagnerMachado84@hotmail.com>
2025-05-10 13:18:40 -0400

    misc lifecycle work

--- 5. deep-dives/src/app/dashboard/tickets/tickets.component.ts
+++ 5. deep-dives/src/app/dashboard/tickets/tickets.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { Ticket } from './ticket/ticket.model';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tickets',
@@ -9,5 +11,18 @@ import { NewTicketComponent } from "./new-ticket/new-ticket.component";
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
+  tickets: Ticket[] = []
+  
+  onAdd(ticketData : { title: string, text: string })
+  {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request:  ticketData.text,
+      id: Math.random(), //.toString(36),
+      status: 'open'
+    };
+    this.tickets.push(ticket);
+    console.log('Ticket added!', this.tickets);
+  }
 
 }

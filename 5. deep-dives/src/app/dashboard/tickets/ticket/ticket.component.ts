b5c4db953a7ecce91394cf830fdf92c072405ae9
Vagner Machado <VagnerMachado84@hotmail.com>
2025-05-18 22:28:56 -0400

    work on new ticket

--- 5. deep-dives/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 5. deep-dives/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from './ticket.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  close = output()
+  detailsVisible = signal(false);
+  
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible)
+  }
 
+  onMarkAsCompleted()
+  {
+    this.close.emit();
+  }
 }
